1cba456c4b99193a56833e0a4331842183fd9113
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-11-18 16:40:39 +0100

    feat: setup mongoose resolver & config

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { LunchGroupModule } from '@modules/lunch-group/lunch-group.module';
+import appConfig from '@config/app.config';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from '@modules/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServicesModule } from './modules/services/services.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('app.database.url'),
+        useNewUrlParser: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    LunchGroupModule,
+    ServicesModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
