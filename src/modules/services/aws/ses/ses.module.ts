22a3049969275a1c7c2effa25d8f6c28fcc350fb
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-11-20 12:41:43 +0100

    feat: initialize AWS module & implement AWS config system

---
+++ src/modules/services/aws/ses/ses.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SesService } from './ses.service';
+import { SesService } from './ses.service';
+
+@Module({
+  providers: [SesService]
+})
+export class SesModule {}
