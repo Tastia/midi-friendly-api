9f2672d1e5c74df1bc60120ff139c3bffb7ff533
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-11-21 20:37:11 +0100

    feat: implement place image stream endpoint

---
+++ src/modules/services/google-maps/google-maps.controller.ts
@@ -0,0 +1,18 @@
+import { GoogleMapsService } from './google-maps.service';
+import { Controller, Get, Param, Query, ValidationPipe } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('services/google-maps')
+@ApiTags('Google Maps')
+export class GoogleMapsController {
+  constructor(private readonly googleMapsService: GoogleMapsService) {}
+
+  @Get('resolve-place-photo/:photoReference')
+  resolvePlacePhoto(
+    @Param('photoReference') photoReference: string,
+    @Query(new ValidationPipe({ transform: true }))
+    options: { maxWidth?: number; maxHeight?: number },
+  ) {
+    return this.googleMapsService.serveImageFromGMapsRef(photoReference, options);
+  }
+}
