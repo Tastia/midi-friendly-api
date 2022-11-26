72419a2900166b4188a586d61d980421ace4b61c
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-11-26 15:41:09 +0100

    feat: add endpoint to retrive restaurants by organization

--- src/modules/restaurant/restaurant.controller.ts
+++ src/modules/restaurant/restaurant.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { RestaurantService } from '@modules/restaurant/restaurant.service';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Restaurant')
 @Controller('restaurant')
-export class RestaurantController {}
+export class RestaurantController {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Get(':organizationId')
+  async getRestaurants(@Param('organizationId') organizationId: string) {
+    return this.restaurantService.find({ organization: organizationId });
+  }
+}
--- src/modules/restaurant/restaurant.service.ts
+++ src/modules/restaurant/restaurant.service.ts
@@ -2,7 +2,8 @@ import { BaseRestaurant } from '@common/types/restaurant';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Restaurant, RestaurantDocument } from '@schemas/restaurant.schema';
-import { Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types } from 'mongoose';
+import { PopulateQuery } from '@common/types/mongoose';
 
 @Injectable()
 export class RestaurantService {
@@ -28,5 +29,13 @@ export class RestaurantService {
         }),
       ),
     );
+  }
+
+  find(filter?: FilterQuery<RestaurantDocument>, populate?: PopulateQuery) {
+    return this.restaurantModel.find(filter ?? {}).populate(populate ?? ('' as any));
+  }
+
+  findOne(filter?: FilterQuery<RestaurantDocument>, populate?: PopulateQuery) {
+    return this.restaurantModel.findOne(filter ?? {}).populate(populate ?? ('' as any));
   }
 }
