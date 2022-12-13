07499c33574f7390cf142783364907f12261d64d
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-13 14:21:53 +0100

    fix: encrypt userId / password only when defined

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -41,12 +41,12 @@ UserSchema.virtual('email').get(function (this: UserDocument) {
 });
 
 UserSchema.pre<UserDocument>('save', function (next) {
-  if (this.isModified('credentials.password'))
+  if (this.isModified('credentials.password') && (this.credentials as EmailCredentials).password)
     (this.credentials as EmailCredentials).password = hashPassword(
       (this.credentials as EmailCredentials).password,
     );
 
-  if (this.isModified('credentials.userId'))
+  if (this.isModified('credentials.userId') && (this.credentials as ProviderCredentials).userId)
     (this.credentials as ProviderCredentials).userId = hashPassword(
       (this.credentials as ProviderCredentials).userId,
     );
