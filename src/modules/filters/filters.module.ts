b0d3188f6966a143a9b7e68a9401376c3fc79817
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-15 17:36:22 +0100

    feat: implement core of filters module

--- src/modules/filters/filters.module.ts
+++ src/modules/filters/filters.module.ts
@@ -1,7 +1,12 @@
+import { OrganizationSchema, Organization } from '@schemas/oraganization.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { FiltersService } from './filters.service';
+import { FiltersController } from './filters.controller';
 
 @Module({
-  providers: [FiltersService]
+  imports: [MongooseModule.forFeature([{ name: Organization.name, schema: OrganizationSchema }])],
+  providers: [FiltersService],
+  controllers: [FiltersController],
 })
 export class FiltersModule {}
