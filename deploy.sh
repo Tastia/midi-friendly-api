11b3e73df64e91515310028dd955d228811e02b3
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-20 13:28:08 +0100

    hotfix: clear docker builder cache on new build

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 sudo docker ps -q
-sudo docker stop $(docker ps -q)
-sudo docker rm $(docker ps -a -q)
-sudo docker images --filter dangling=true -q
-sudo docker rmi $(docker images --filter dangling=true -q)
-sudo docker images -a -q
-sudo docker rmi $(docker images -a -q)
+sudo docker builder prune -f
 sudo docker container prune -f
 sudo docker image prune -f
 sudo docker system prune -f
