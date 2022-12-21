0c57ccabb22535f46c169c46f473857c760ba009
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-21 02:12:01 +0100

    chore: add docker cache forced cleanup on CI

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
-sudo docker ps -q
-sudo docker builder prune -f
+sudo docker-compose down
+sudo docker images -f dangling=true
+sudo docker system prune -f
 sudo docker container prune -f
 sudo docker image prune -f
+sudo docker builder prune -f
 sudo docker system prune -f
 sudo docker-compose up -d --build
 sudo nginx -s reload
