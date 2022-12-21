2d1367d803fb2ecf48aa5b90e22bb6716ee2b12c
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-21 16:00:50 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ sudo docker-compose down
 sudo docker images -f dangling=true
 sudo docker system prune -f
 sudo docker container prune -f
+sudo docker image prune -a
 sudo docker image prune -f
 sudo docker builder prune -f
 sudo docker system prune -f
