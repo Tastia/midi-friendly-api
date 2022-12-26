beb24878b248498ccad2ecd761647a1916edccce
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-26 19:44:53 +0100

    feat: add organization / user resolver to REST access guard

--- src/common/guards/access.guard.ts
+++ src/common/guards/access.guard.ts
@@ -1,21 +1,30 @@
+import { AuthService } from '@modules/auth/auth.service';
+import { OrganizationService } from '@modules/organization/organization.service';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 
 /**
  * Access guard for Nest authentication mechanism
  */
 @Injectable()
 export class AccessGuard implements CanActivate {
-  private reflector: Reflector;
-  constructor() {
-    this.reflector = new Reflector();
-  }
+  constructor(
+    private readonly organizationService: OrganizationService,
+    private readonly authService: AuthService,
+  ) {}
 
   /**
    * Check if this method can be activated with given execution context
    * @param context
    */
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const organizationId = context.switchToHttp().getRequest().headers.organizationid;
+    const authToken = context.switchToHttp().getRequest().headers.authorization;
+    const user = await this.authService.validateAccessToken(authToken.split(' ')[1]);
+    const organization = await this.organizationService.findOne({ _id: organizationId });
+
+    context.switchToHttp().getRequest().user = user;
+    context.switchToHttp().getRequest().organization = organization;
+
     return true;
   }
 }
