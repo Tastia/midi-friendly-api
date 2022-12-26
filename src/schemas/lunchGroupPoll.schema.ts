dd385c7c546a04af1173cb8c42f8812f05c4af07
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-26 19:43:53 +0100

    feat: implement lunch group poll schema

---
+++ src/schemas/lunchGroupPoll.schema.ts
@@ -0,0 +1,54 @@
+import { Restaurant } from '@schemas/restaurant.schema';
+import { Organization } from './oraganization.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Transform } from 'class-transformer';
+import { User } from './user.schema';
+import { LunchGroupPollEntries, LunchGroupStatus } from '@common/types/lunchGroup';
+import { LunchGroup } from './lunchGroup.schema';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const aggregatePaginate = require('mongoose-aggregate-paginate-v2');
+
+export type LunchGroupPollDocument = LunchGroupPoll & Document;
+
+@Schema({ timestamps: true })
+export class LunchGroupPoll {
+  @Transform(({ value }) => value.toString())
+  _id: mongoose.Types.ObjectId;
+
+  @Prop({ enum: LunchGroupStatus, default: LunchGroupStatus.open })
+  status: LunchGroupStatus;
+
+  @Prop()
+  label: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop()
+  voteDeadline: string;
+
+  @Prop()
+  meetingTime: string;
+
+  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' }])
+  allowedRestaurants?: Restaurant[];
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Organization' })
+  organization: Organization;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  owner: User;
+
+  @Prop({ type: [LunchGroupPollEntries], default: [] })
+  votes: LunchGroupPollEntries[];
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'LunchGroup' })
+  lunchGroup?: LunchGroup;
+}
+
+const LunchGroupPollSchema = SchemaFactory.createForClass(LunchGroupPoll);
+
+LunchGroupPollSchema.plugin(aggregatePaginate);
+
+export { LunchGroupPollSchema };
